97654e7447a16e927026bedd36ac98fc1126eb3a
Evan Greer <evan.greer@iterable.com>
2022-10-27 16:05:41 -0600

    sets up navigation to and implementation of the asteroid detail screen

--- app/src/main/java/com/udacity/asteroidradar/main/AsteroidAdapter.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/AsteroidAdapter.kt
@@ -2,28 +2,17 @@ package com.udacity.asteroidradar.main
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.udacity.asteroidradar.Asteroid
-import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.ListItemAsteroidBinding
 
-class AsteroidAdapter: ListAdapter<Asteroid,
-        AsteroidAdapter.ViewHolder>(AsteroidDiffCallback()) {
+class AsteroidAdapter(val onClickListener: OnClickListener): ListAdapter<Asteroid,
+        AsteroidAdapter.ViewHolder>(AsteroidDiffCallback) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder.from(parent)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = getItem(position)
-        holder.bind(item)
-    }
-
-    class ViewHolder private constructor(val binding: ListItemAsteroidBinding): RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder private constructor (private val binding: ListItemAsteroidBinding):
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: Asteroid) {
             binding.asteroid = item
@@ -38,14 +27,32 @@ class AsteroidAdapter: ListAdapter<Asteroid,
             }
         }
     }
-}
 
-class AsteroidDiffCallback : DiffUtil.ItemCallback<Asteroid>() {
-    override fun areItemsTheSame(oldItem: Asteroid, newItem: Asteroid) : Boolean {
-        return oldItem.id == newItem.id
+    companion object AsteroidDiffCallback : DiffUtil.ItemCallback<Asteroid>() {
+        override fun areItemsTheSame(oldItem: Asteroid, newItem: Asteroid) : Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Asteroid, newItem: Asteroid) : Boolean {
+            return oldItem == newItem
+        }
+
+
     }
 
-    override fun areContentsTheSame(oldItem: Asteroid, newItem: Asteroid) : Boolean {
-        return oldItem == newItem
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val asteroid = getItem(position)
+        holder.itemView.setOnClickListener {
+            onClickListener.onClick(asteroid)
+        }
+        holder.bind(asteroid)
+    }
+
+    class OnClickListener(val clickListener: (asteroid: Asteroid) -> Unit) {
+        fun onClick(asteroid: Asteroid) = clickListener(asteroid)
     }
 }
--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -6,6 +6,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.udacity.asteroidradar.Asteroid
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.data.source.local.AsteroidDatabase
@@ -23,97 +24,22 @@ class MainFragment : Fragment() {
         val binding: FragmentMainBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_main, container, false)
 
-        val application = requireNotNull(this.activity).application
+        val adapter = AsteroidAdapter(AsteroidAdapter.OnClickListener {
+            viewModel.displayAsteroidDetails(it)
+        })
 
-        val asteroids = listOf<Asteroid>(
-            Asteroid(
-                id = 1,
-                codename = "(2015 RC)",
-                closeApproachDate = "2015-09-22",
-                absoluteMagnitude = 0.5034568,
-                estimatedDiameter = 3.4445168,
-                relativeVelocity = 18.1234568,
-                distanceFromEarth = 0.3027479,
-                isPotentiallyHazardous = true
-            ),
-            Asteroid(
-                id = 2,
-                codename = "(2016 RC)",
-                closeApproachDate = "2016-12-12",
-                absoluteMagnitude = 1.3456232,
-                estimatedDiameter = 4.0034531,
-                relativeVelocity = 22.1112345,
-                distanceFromEarth = 0.7511675,
-                isPotentiallyHazardous = false
-            ),
-            Asteroid(
-                id = 3,
-                codename = "(2017 RC)",
-                closeApproachDate = "2017-11-18",
-                absoluteMagnitude = 3.3312447,
-                estimatedDiameter = 2.0224514,
-                relativeVelocity = 18.2334545,
-                distanceFromEarth = 1.2214315,
-                isPotentiallyHazardous = true
-            ),
-            Asteroid(
-                id = 4,
-                codename = "(2020 RC)",
-                closeApproachDate = "2020-10-01",
-                absoluteMagnitude = 3.3312447,
-                estimatedDiameter = 2.0224514,
-                relativeVelocity = 18.2334545,
-                distanceFromEarth = 1.2214315,
-                isPotentiallyHazardous = true
-            ),
-            Asteroid(
-                id = 4,
-                codename = "(2020 RC)",
-                closeApproachDate = "2020-10-01",
-                absoluteMagnitude = 3.3312447,
-                estimatedDiameter = 2.0224514,
-                relativeVelocity = 18.2334545,
-                distanceFromEarth = 1.2214315,
-                isPotentiallyHazardous = true
-            ),
-            Asteroid(
-                id = 4,
-                codename = "(2020 RC)",
-                closeApproachDate = "2020-10-01",
-                absoluteMagnitude = 3.3312447,
-                estimatedDiameter = 2.0224514,
-                relativeVelocity = 18.2334545,
-                distanceFromEarth = 1.2214315,
-                isPotentiallyHazardous = true
-            ),
-            Asteroid(
-                id = 4,
-                codename = "(2020 RC)",
-                closeApproachDate = "2020-10-01",
-                absoluteMagnitude = 3.3312447,
-                estimatedDiameter = 2.0224514,
-                relativeVelocity = 18.2334545,
-                distanceFromEarth = 1.2214315,
-                isPotentiallyHazardous = true
-            ),
-            Asteroid(
-                id = 4,
-                codename = "(2020 RC)",
-                closeApproachDate = "2020-10-01",
-                absoluteMagnitude = 3.3312447,
-                estimatedDiameter = 2.0224514,
-                relativeVelocity = 18.2334545,
-                distanceFromEarth = 1.2214315,
-                isPotentiallyHazardous = true
-            )
-        )
-
-        val adapter = AsteroidAdapter()
         binding.asteroidRecycler.adapter = adapter
 
         viewModel.asteroids.observe(viewLifecycleOwner, Observer {
             it?.let {
                 adapter.submitList(it)
+            }
+        })
+
+        viewModel.navigateToSelectedAsteroid.observe(viewLifecycleOwner, Observer {
+            if ( null != it ) {
+                this.findNavController().navigate(MainFragmentDirections.actionShowDetail(it))
+                viewModel.displayAsteroidDetailsComplete()
             }
         })
 
